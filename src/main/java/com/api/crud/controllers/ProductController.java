c386a427adf2df9e014ea39bc284caa59c7c2154
Daniel Penelva <d4n.andrade@gmail.com>
2023-12-26 22:13:25 -0300

    Criando os métodos findByIdProduct, updateProduct e updateProduct1 e deleteProduct da API Rest Controller

--- src/main/java/com/api/crud/controllers/ProductController.java
+++ src/main/java/com/api/crud/controllers/ProductController.java
@@ -3,10 +3,15 @@ package com.api.crud.controllers;
 import com.api.crud.domain.Product;
 import com.api.crud.domain.RequestProductDto;
 import com.api.crud.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/product")
@@ -22,12 +27,71 @@ public class ProductController {
         return ResponseEntity.ok(allProducts);
     }
 
+    // localhost:8080/product/search/{id}
+    @GetMapping("/search/{id}")
+    public ResponseEntity findByIdProduct(@PathVariable String id){
+        Product idProduct = productRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Product not found with id: " + id));
+        return ResponseEntity.ok(idProduct);
+    }
+
     // localhost:8080/product/create
     @PostMapping("/create")
-    public ResponseEntity registerProduct(@RequestBody @Valid RequestProductDto data){
+    @Transactional
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProductDto data) {
         Product newProduct = new Product(data);
         productRepository.save(newProduct);
         return ResponseEntity.ok().body("Product created successfully!");
         // ou // return ResponseEntity.ok().build();
+    }
+
+    // localhost:8080/product/update
+    @PutMapping("/update")
+    @Transactional
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProductDto data) {
+
+        Optional<Product> optionalProduct = productRepository.findById(data.id());
+
+        if (optionalProduct.isPresent()) {
+            Product updateProduct = optionalProduct.get();
+            updateProduct.setName(data.name());
+            updateProduct.setPrice_in_cents(data.price_in_cents());
+            productRepository.save(updateProduct);
+            return ResponseEntity.ok(updateProduct);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // localhost:8080/product/replace
+    @PutMapping("/replace")
+    @Transactional
+    public ResponseEntity updateProduct1(@RequestBody @Valid RequestProductDto data) {
+
+        UUID productId = UUID.fromString(data.id());
+
+        Product updateProduct1 = productRepository.findById(productId.toString())
+                .orElseThrow(() -> new EntityNotFoundException("Product not found with id: " + productId));
+
+        updateProduct1.setName(data.name());
+        updateProduct1.setPrice_in_cents(data.price_in_cents());
+
+        productRepository.save(updateProduct1);
+        return ResponseEntity.ok(updateProduct1);
+    }
+
+    // localhost:8080/product/delete/{id}
+    @DeleteMapping("delete/{id}")
+    @Transactional
+    public ResponseEntity deleteProduct(@PathVariable String id){
+        Optional<Product> optionalProduct = productRepository.findById(id);
+
+        if(optionalProduct.isPresent()){
+            Product productDelete = optionalProduct.get();
+            productRepository.delete(productDelete);
+            return ResponseEntity.noContent().build();
+
+        }else{
+            throw  new EntityNotFoundException();
+        }
     }
 }
--- src/main/java/com/api/crud/domain/RequestProductDto.java
+++ src/main/java/com/api/crud/domain/RequestProductDto.java
@@ -4,5 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record RequestProductDto(
+        String id,
         @NotBlank String name,
         @NotNull Integer price_in_cents) { }
